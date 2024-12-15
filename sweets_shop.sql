5bba6b6b482edd30b39d9fa4f1dd9dd0f11372c6
unknown <al.zhivkova@gmail.com>
2024-12-15 22:36:45 +0200

    Added database selection

--- sweets_shop.sql
+++ sweets_shop.sql
@@ -24,7 +24,8 @@ CREATE DATABASE sweets_shop;
 CREATE USER 'sweetsshop_admin'@'localhost' IDENTIFIED BY 'Sweets_2024';
 GRANT ALL PRIVILEGES ON sweets_shop.* TO `sweetsshop_admin`@`localhost`;
 
-
+USE sweets_shop
+GO;
 -- --------------------------------------------------------
 
 --
